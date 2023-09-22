0f78aed576283de8e2982bd9b1e30c457faba99f
Jefferson Mattos <jefflmattos@gmail.com>
2023-09-22 20:32:10 +0000

    webpart

---
+++ src/webparts/birthdayCard/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IBirthdayCardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'BirthdayCardWebPartStrings' {
+  const strings: IBirthdayCardWebPartStrings;
+  export = strings;
+}
